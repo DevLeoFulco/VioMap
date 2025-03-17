bcdadef3632bcd9de33bd986f52cfd0f04f4f395
DevLeoFulco <devleofulco@gmail.com>
2025-03-17 20:10:55 -0300

    feature: "adicionado-rotas"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+      },
+      {
+        path: 'map',
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'report',
+        loadComponent: () => import('./features/incident-form/incident-form.component').then(m => m.IncidentFormComponent)
+      },
+      {
+        path: 'statistics',
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      }
+];
